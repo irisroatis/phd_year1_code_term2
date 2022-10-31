03539dd306f567803a4ffd53328c2832de148e9c
Iris-Ioana Roatis <iris-ioana.roatis18@imperial.ac.uk>
2022-10-31 23:25:20 +0000

    taking functions out

--- lda_binning.py
+++ lda_binning.py
@@ -14,56 +14,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import scipy.stats
 from mpl_toolkits import mplot3d
 import random
-
-####### FUNCTIONS NEEDED
-
-def put_in_bins(data, bins):
-    digitized = np.digitize(data,bins)
-    midpoints_bins = (bins[:len(bins)-1] + bins[1:])/2
-    new_data = midpoints_bins[digitized-1]
-    return new_data
-
-
-
-def lda_find_hyperplane(s1,s2):
-    
-    size1 = len(s1)
-    size2 = len(s2)
-    
-    # estimating mu by doing average
-    estimated_mu_1 = np.array([np.mean(s1)])
-    estimated_mu_2 = np.array([np.mean(s2)])
-
-    # estimating sigma by doing sample variance
-    size1min1 = size1-1
-    size2min1 = size2-1
-    estimated_sigma_1 = np.array([np.sum((s1 - estimated_mu_1)**2)/size1min1])
-    estimated_sigma_2 = np.array([np.sum((s2 - estimated_mu_2)**2)/size2min1]) 
-
-    sigma = 1/ (size1+ size2 - 2) *(size1min1 * estimated_sigma_1 + size2min1 * estimated_sigma_2)
-
-    log_ratio_proportions = np.log(size2 / size1)
-    difference_in_est_means = estimated_mu_1 - estimated_mu_2
-
-
-    # define the alpha (normal to the boundary plane)
-    if sigma.shape != (1,):
-        alpha = np.linalg.inv(sigma) * difference_in_est_means
-        c = 0.5 * np.traspose(alpha) * (estimated_mu_1 + estimated_mu_2) + log_ratio_proportions
-    else:
-        alpha = 1/sigma * difference_in_est_means
-        c = 0.5 * alpha * (estimated_mu_1 + estimated_mu_2) + log_ratio_proportions
-    
-    return alpha, c
-    
-
-def lda_predict(x, alpha, c):
-    if np.transpose(alpha) * x <= c:
-        predicted_class = 2
-    else:
-        predicted_class = 1
-    return predicted_class
-
+from defined_functions import *
 
 
 ####### START OF THE CODE
@@ -84,7 +35,7 @@ dim = len(mu1)
 iterations = 5000
 how_many_times_repeat = 30
 
-list_nt = [2,5, 15, 40, 85, 200, 500]
+list_nt = [2, 5, 15, 40, 85, 200, 500]
 list_b = [2, 1, 0.5,0.35, 0.2, 0.1, 0.075, 0.05, 0.0001]
 
 # keeping track on which iteration we are on
