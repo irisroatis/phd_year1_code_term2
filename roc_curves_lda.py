012e8847e4867d9b0e70a242363aad9d6565d3d5
Iris-Ioana Roatis <iris-ioana.roatis18@imperial.ac.uk>
2022-11-01 15:23:08 +0000

    updated code with curves for multiple parameter options

--- roc_curves_lda.py
+++ roc_curves_lda.py
@@ -27,16 +27,16 @@ from defined_functions import *
 
 mu1 = np.array([0.25]) # mean and standard deviation
 sigma1 = np.array([1]) # mean and standard deviation
-mu2 = np.array([1]) # mean and standard deviation
+mu2 = np.array([1.25]) # mean and standard deviation
 sigma2 = np.array([1]) # mean and standard deviation
 
 dim = len(mu1)
 
-iterations = 5000
-how_many_times_repeat = 30
+iterations = 100
+how_many_times_repeat = 5
 
 value_b = 3
-value_nt = 3
+value_nt = 20
 
 # keeping track on which iteration we are on
 progress = 0
@@ -110,55 +110,145 @@ s2_binned = put_in_bins(s2, bins)
 
 alpha, c = lda_find_hyperplane(s1_binned,s2_binned)
 # print(str(alpha) + str(c))
-tpr, fpr = list(), list()
+list_tpr, list_fpr = list(), list()
+# list_tpr_b, list_fpr_b = list(), list()
 
-try_values_for_c = np.linspace(-3,3,20)
+try_values_for_c = np.linspace(-20,20,200)
 
 for c in try_values_for_c:
     
-    avg_tpr = 0
-    avg_fpr = 0
+    tpr = np.zeros((how_many_times_repeat,))
+    fpr = np.zeros((how_many_times_repeat,))
+    
+    # tpr_b = np.zeros((how_many_times_repeat,))
+    # fpr_b = np.zeros((how_many_times_repeat,))
+    
     
     for repeat in range(how_many_times_repeat):
         
         testing_model_on_binned = put_in_bins(testing_data[repeat],bins)
         testing_model_on_unbinned = testing_data[repeat]
         correct_classes = belonging_classes[repeat]
-        
+
         true_pos = 0
         true_neg = 0
         false_pos = 0
         false_neg = 0
         
+        predicted_classes_unbinnned = np.zeros((iterations,))
+        predicted_classes_binnned = np.zeros((iterations,))
         for itera in range(iterations):
-            current_predicted_class = lda_predict(testing_model_on_unbinned[itera,], alpha, c)
-            
-            if correct_classes[itera,] == 2 and current_predicted_class == 1 :
-                false_pos += 1
-            if correct_classes[itera,] == 1 and current_predicted_class == 1 :
-                true_pos += 1
-            if correct_classes[itera,] == 1 and current_predicted_class == 2 :
-                false_neg += 1
-            if correct_classes[itera,]== 2 and current_predicted_class == 2 :
-                true_neg += 1
+            predicted_classes_unbinnned[itera,] = lda_predict(testing_model_on_unbinned[itera,], alpha, c)
+            # predicted_classes_binnned[itera,] = lda_predict(testing_model_on_binned[itera,], alpha, c)
+
         
-        ##### THINK ABOUT WHAT TO DO WITH THESE
-        
-        avg_tpr +=  true_pos / (true_pos + false_neg)
-        avg_fpr += false_pos / (false_pos + false_neg) 
+        tpr[repeat,], fpr[repeat,] = tprfpr(correct_classes, predicted_classes_unbinnned)
+        # tpr_b[repeat,], fpr_b[repeat,] = tprfpr(correct_classes, predicted_classes_binnned)
         
     
+    list_tpr.append(tpr)
+    list_fpr.append(fpr)
     
-    tpr.append( avg_tpr / how_many_times_repeat )
-    fpr.append (avg_fpr / how_many_times_repeat  )
+    # list_tpr_b.append(tpr_b)
+    # list_fpr_b.append(fpr_b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bin_size = 10
+
+# bins are created around 0 (one bin is set with ends in 0 and outwards
+# from there)
+bins = np.arange(0,100,bin_size)
+bins = np.concatenate((-bins[::-2], bins))
+
+s1_binned = put_in_bins(s1, bins)
+s2_binned = put_in_bins(s2, bins)
+
+     
+## this is our own implementation of 'trainig the model' using LDA
+
+alpha, c = lda_find_hyperplane(s1_binned,s2_binned)
+# print(str(alpha) + str(c))
+list_tpr_2, list_fpr_2 = list(), list()
+# list_tpr_b, list_fpr_b = list(), list()
+
+try_values_for_c = np.linspace(-20,20,200)
+
+for c in try_values_for_c:
+    
+    tpr = np.zeros((how_many_times_repeat,))
+    fpr = np.zeros((how_many_times_repeat,))
+    
+    # tpr_b = np.zeros((how_many_times_repeat,))
+    # fpr_b = np.zeros((how_many_times_repeat,))
+    
+    
+    for repeat in range(how_many_times_repeat):
+        
+        testing_model_on_binned = put_in_bins(testing_data[repeat],bins)
+        testing_model_on_unbinned = testing_data[repeat]
+        correct_classes = belonging_classes[repeat]
+
+        true_pos = 0
+        true_neg = 0
+        false_pos = 0
+        false_neg = 0
+        
+        predicted_classes_unbinnned = np.zeros((iterations,))
+        predicted_classes_binnned = np.zeros((iterations,))
+        for itera in range(iterations):
+            predicted_classes_unbinnned[itera,] = lda_predict(testing_model_on_unbinned[itera,], alpha, c)
+            # predicted_classes_binnned[itera,] = lda_predict(testing_model_on_binned[itera,], alpha, c)
+
+        
+        tpr[repeat,], fpr[repeat,] = tprfpr(correct_classes, predicted_classes_unbinnned)
+        # tpr_b[repeat,], fpr_b[repeat,] = tprfpr(correct_classes, predicted_classes_binnned)
+        
+    
+    list_tpr_2.append(tpr)
+    list_fpr_2.append(fpr)
+    
+    # list_tpr_b.append(tpr_b)
+    # list_fpr_b.append(fpr_b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 x = np.linspace(0,1,100)
 
 
 
-
-
 plt.figure()
-plt.plot(fpr,tpr,'.-')
+for i in range(len(list_tpr)):
+    plt.plot(np.mean(list_fpr[i]),np.mean(list_tpr[i]),'r.-')
+    plt.plot(np.mean(list_fpr_2[i]),np.mean(list_tpr_2[i]),'y.-')
 plt.plot(x,x)
+plt.xlabel('fpr')
+plt.ylabel('tpr')
+plt.title('$n_t$ = '+str(value_nt) +', $b$ = '+str(value_b))
 plt.show()
