29974ef9a6de7ad94c4d148bb6f3eaba45b39b1c
Iris-Ioana Roatis <iris-ioana.roatis18@imperial.ac.uk>
2022-11-08 17:14:59 +0000

    trying to prove shepards corrections

--- checking_test_statistics_binned.py
+++ checking_test_statistics_binned.py
@@ -10,37 +10,32 @@ import numpy as np
 from defined_functions import *
 import matplotlib.pyplot as plt
 
-mu = 0.25
-sigma = 1
+mu = 1
+sigma = 3
 
-list_nt = [50, 100, 500, 1000, 5000, 10000, 50000]
-bin_size = 2
+list_nt = [5, 50, 100, 500, 1000, 5000, 10000, 50000]
+bin_size = 1
 bins = np.arange(0,100,bin_size)
-bins = np.concatenate((-bins[::-2], bins))
+bins = np.concatenate((-bins[::-1], bins))
+# bins = np.linspace(-100,100,int(200//bin_size))
 how_many = 100
-list_mean = []
 list_mean_binned = []
 list_difference = []
+list_sample_variance = []
 
 for nt in list_nt:
-    sample_mean = np.zeros((how_many,))
     sample_mean_binned = np.zeros((how_many,))
+    sample_variance = np.zeros((how_many,))
     for iterations in range(how_many):
         random_simulation_from_normal =  np.random.normal(mu, sigma, nt)
+
         binned_simulted_data = put_in_bins(random_simulation_from_normal, bins)
-        sample_mean[iterations,] = np.mean(random_simulation_from_normal)
         sample_mean_binned[iterations,] = np.mean(binned_simulted_data)
-    list_mean.append(sample_mean)
+        sample_variance[iterations,] = sum((binned_simulted_data -  sample_mean_binned[iterations,])**2) / (nt-1) 
     list_mean_binned.append(sample_mean_binned)
     list_difference.append(sample_mean_binned-mu)
+    list_sample_variance.append(sample_variance)
     
-fig, ax = plt.subplots()
-ax.boxplot(list_mean)
-ax.set_xticklabels(list_nt)
-plt.title('Boxplots of estimated means from unbinned data')
-plt.axhline(mu,color = 'g',linestyle = '--',linewidth = 1)
-plt.show()
-
 fig, ax = plt.subplots()
 ax.boxplot(list_mean_binned)
 ax.set_xticklabels(list_nt)
@@ -52,5 +47,12 @@ fig, ax = plt.subplots()
 ax.boxplot(list_difference)
 ax.set_xticklabels(list_nt)
 plt.axhline(0,color = 'g',linestyle = '--',linewidth = 1)
-plt.title('Boxplots of estimated means from binned data')
+plt.title('Boxplots of bias of mean')
+plt.show()
+
+fig, ax = plt.subplots()
+ax.boxplot(list_sample_variance)
+ax.set_xticklabels(list_nt)
+plt.axhline(sigma+ bin_size**2/12,color = 'g',linestyle = '--',linewidth = 1)
+plt.title('Boxplots of sample variances about the estimated mean')
 plt.show()
