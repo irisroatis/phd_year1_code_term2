183b3f623b115675f85b47efb881e0a487ed771d
Iris-Ioana Roatis <iris-ioana.roatis18@imperial.ac.uk>
2022-11-11 14:45:51 +0000

    slightly change the roc curves plot

--- kNN_implementation.py
+++ kNN_implementation.py
@@ -13,73 +13,63 @@ import matplotlib.pyplot as plt
 mu1, sigma1 = [0], [1] # mean and standard deviation
 mu2, sigma2 = [1], [1] # mean and standard deviation
 
-# uncomment for random sizes out of the two simulations
-# size1 = random.randint(10**4, 10**6)
-# size2 = random.randint(10**4, 10**6)
 
-nt = 300
-bin_size = 0.5
+how_many_times_repeat = 1
+iterations = 1000
+list_of_thresholds = np.linspace(0, 1, 50)
 
-# generate from normal distrib
-s1 = np.random.normal(mu1, sigma1, nt)
-s2 = np.random.normal(mu2, sigma2, nt)
-
-
-bins = np.arange(0,100,bin_size)
-bins = np.concatenate((-bins[::-1], bins))
-
-# bins = np.linspace(-100,100,int(200//bin_size))
-
-
-s1_binned = put_in_bins(s1, bins)
-s2_binned = put_in_bins(s2, bins)
-
-
-
-
-# create X and y to apply LDA
-X = np.concatenate((s1_binned.reshape((nt,1)),s2_binned.reshape((nt,1))))
-y = np.concatenate((np.ones((nt,)),2*np.ones((nt,))))
-
-
-
-how_many_times_repeat = 5
-iterations = 100
-
-
+#generating test data
 testing_data, belonging_classes = generating_test_data(how_many_times_repeat, 
                                                         iterations, mu1, sigma1, 
                                                         mu2, sigma2, 
-                                                        plot_classes = True)
+                                                        plot_classes = False)
+
+###### First Set of Parameters
+nt1 = 400
+bin_size1 = 0.01
+k1 = 71
+bins = np.arange(bin_size1/2,100,bin_size1)
+bins = np.concatenate((-bins[::-1], bins))
+# bins = np.linspace(-100,100,int(200//bin_size))
+
+s1 = np.random.normal(mu1, sigma1, nt1)
+s2 = np.random.normal(mu2, sigma2, nt1)
+s1_binned = put_in_bins(s1, bins) #binned
+s2_binned = put_in_bins(s2, bins) #binned
+# create X and y to apply kNN
+X = np.concatenate((s1_binned.reshape((nt1,1)),s2_binned.reshape((nt1,1))))
+y = np.concatenate((np.ones((nt1,)),2*np.ones((nt1,))))
+list_tpr, list_fpr, list_accuracy = roc_kNN(X, y, testing_data, belonging_classes, list_of_thresholds, how_many_times_repeat, k1)
 
 
-list_of_thresholds = np.linspace(0, 1, 100)
-list_fpr = []
-list_tpr = []
-list_accuracy = []
+
+###### Second Set of Parameters
+nt2 = 400
+bin_size2 = 0.01
+k2 = 5
+bins = np.arange(bin_size2/2,100,bin_size2)
+bins = np.concatenate((-bins[::-1], bins))
+# bins = np.linspace(-100,100,int(200//bin_size))
+
+s1_2 = np.random.normal(mu1, sigma1, nt2)
+s2_2 = np.random.normal(mu2, sigma2, nt2)
+s1_2_binned = put_in_bins(s1_2, bins) #binned
+s2_2_binned = put_in_bins(s2_2, bins) #binned
+# create X and y to apply kNN
+X2 = np.concatenate((s1_2_binned.reshape((nt2,1)),s2_2_binned.reshape((nt2,1))))
+y2 = np.concatenate((np.ones((nt2,)),2*np.ones((nt2,))))
+list_tpr2, list_fpr2, list_accuracy2 = roc_kNN(X2, y2, testing_data, belonging_classes, list_of_thresholds, how_many_times_repeat, k2)
 
 
-
-for t in list_of_thresholds:
-    tpr = np.zeros([how_many_times_repeat,])
-    fpr = np.zeros([how_many_times_repeat,])
-    accuracy = np.zeros([how_many_times_repeat,])
-    for repeat in range(how_many_times_repeat):
-        predicted_classes_kNN = reg_predict(X, testing_data[repeat], y, k=71, threshold=t)
-        results = tprfpr(belonging_classes[repeat], predicted_classes_kNN, accuracy = True)
-        tpr[repeat] = results[0]
-        fpr[repeat] = results[1]
-        accuracy[repeat] = results[2]
-    list_tpr.append(tpr)
-    list_fpr.append(fpr)
-    list_accuracy.append(accuracy)   
 
 
 x = np.linspace(0,1,100)
 plt.figure()
-for i in range(len(list_tpr)):
-    plt.plot(np.mean(list_fpr[i]),np.mean(list_tpr[i]),'r.-')
+
+plt.plot(list_fpr,list_tpr,'r.-',label='$n_t=$'+str(nt1)+',$b=$'+str(bin_size1)+',$k=$'+str(k1))
+plt.plot(list_fpr2,list_tpr2,'y.-',label='$n_t=$'+str(nt2)+',$b=$'+str(bin_size2)+',$k=$'+str(k2))
 plt.plot(x,x)
+plt.legend()
 plt.xlabel('fpr')
 plt.ylabel('tpr')
 # plt.title('$n_t$ = '+str(value_nt) +', $b$ = '+str(value_b))
@@ -95,3 +85,4 @@ plt.show()
 # predicted_kNN_builtin = regressor.predict(testing_data[0]).tolist()
 
 # print(predicted_kNN_builtin == predicted_classes_kNN)
+
