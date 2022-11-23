e04a6558de877096a1365d8c24960901d2dd1fc8
Iris-Ioana Roatis <iris-ioana.roatis18@imperial.ac.uk>
2022-11-23 15:57:07 +0000

    logistic classification working

--- logistic_implem.py
+++ logistic_implem.py
@@ -6,70 +6,157 @@ Created on Fri Nov 11 10:47:31 2022
 @author: ir318
 """
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+import random
+import matplotlib.pyplot as plt
+
+def generating_test_data(how_many_times_repeat, iterations, mu1, sigma1, mu2, 
+                         sigma2, plot_classes = False):
+    """
+
+    Parameters
+    ----------
+    how_many_times_repeat : how many different samples wanted (how many
+                                                               experiments)
+    iterations : sample size (how many realisations in a single experiment)
+    mu1 : mean of first Normal
+    sigma1 : variance of first Normal
+    mu2 : mean of second Normal
+    sigma2 : variance of second Normal
+
+    Returns
+    -------
+    testing_data : a list of arrays. Each array is an experiment who has 
+                   multiple realisations from both Normals
+    belonging_classes : the true classes of the samples from the experiments
+                        (the true normal each realisation comes from)
+
+    """
+    
+    dim = len(mu1)
+    testing_data=[]
+    belonging_classes=[]
+
+    for repeat in range(how_many_times_repeat):
+
+        random_simulation = np.zeros((iterations,dim))
+        which_class_list = np.zeros((iterations,))
+        
+        for itera in range(iterations):
+
+            which_normal = random.randint(0,1)
+            if dim == 1:
+                if which_normal == 0:
+                    random_simulation[itera,] = np.random.normal(mu1, sigma1)
+                else:
+                    random_simulation[itera,] = np.random.normal(mu2, sigma2)
+            else:
+                if which_normal == 0:
+                    random_simulation[itera,] = np.random.multivariate_normal(mu1, sigma1)
+                else:
+                    random_simulation[itera,] = np.random.multivariate_normal(mu2, sigma2)
+            which_class_list[itera,] = which_normal
+        
+        testing_data.append(random_simulation)
+        belonging_classes.append(which_class_list)
+      
+    
+    return testing_data, belonging_classes
+    
+
+def put_in_bins(data, bins):
+    digitized = np.digitize(data,bins)
+    midpoints_bins = (bins[:len(bins)-1] + bins[1:])/2
+    new_data = midpoints_bins[digitized-1]
+    return new_data
 
 def logistic(x):
     logistic =  1/(1+np.exp(-x))
     return logistic
 
-def predict_log(X, beta, beta_0):
-    y_log = logistic(beta.T @ X + beta_0) # Use formula above
-    return y_log
-
-def initialise(d):
-    beta = np.zeros(shape=(d, 1), dtype=np.float32) # Initialise beta
-    beta_0 = 0 # Initialise beta_0
-    return beta, beta_0
-
-def propagate(X, y, beta, beta_0):
-    n = X.shape[1] 
-    y_log = predict_log(X, beta, beta_0) # Find predictions
-    # Find cost function
-    cost = (-1) * np.mean(np.multiply(y,np.log(y_log)) + np.multiply(1-y,np.log(1-y_log)), axis = 1) 
-    cost = np.squeeze(cost)
-    # Find derivatives
-    dbeta = (1/n) * X @ np.transpose(y_log - y)
-    dbeta_0 = np.mean(y_log-y)
-    grads = {"dbeta": dbeta, "dbeta_0": dbeta_0}  # Store gradients in a dictionary
-    return grads, cost
-
-
-
 def optimise(X, y, beta, beta_0, num_iterations, learning_rate=0.005):
-    costs = list() # Initialise empty list of costs 
+    n = X.shape[0] 
     for i in range(num_iterations):
-        # Calculate cost and gradients 
-        grads, cost = propagate(X, y, beta, beta_0)
-        # Retrieve derivatives from dictionary grads
-        dbeta = grads["dbeta"]
-        dbeta_0 = grads["dbeta_0"]  
-        # Updating procedure for the parameters and offset
+        # Calculate gradients 
+        
+        y_log = logistic(X @ beta + beta_0) 
+     
+        dbeta = (1/n) * np.transpose(X) @ (y_log - y)
+        dbeta_0 = np.mean(y_log-y)
+        
+        # Updating procedure for the parameters (gradient descent)
         beta -= learning_rate * dbeta
         beta_0 -= learning_rate * dbeta_0 
-        # Record the costs
-        if i % 100 == 0:
-            costs.append(cost)
+ 
     # Save parameters and gradients in dictionary
     params = {"beta": beta, "beta_0": beta_0}
-    grads = {"dbeta": dbeta, "dbeta_0": dbeta_0}
-    return params, grads, costs
+    return params
+
 
 def predict_logistic(X_test, beta, beta_0, threshold):
-    n = X_test.shape[1]
-    y_pred = np.zeros((1,n))
-    beta = beta.reshape(X_test.shape[0], 1)
+    n = X_test.shape[0]
+    y_pred = np.zeros((n,1))
     # Compute vector y_log predicting the probabilities
-    y_log = predict_log(X_test, beta, beta_0)
-    for i in range(y_log.shape[1]):
+    y_log = logistic(X_test @ beta + beta_0) 
+    for i in range(y_log.shape[0]):
         # Convert probabilities y_log to actual predictions y_pred using the threshold
-        if y_log[0, i] > threshold:
-            y_pred[0, i] = 1  
+        if y_log[i,0] > threshold:
+            y_pred[i,0] = 1  
         else:
-            y_pred[0, i] = 0  
+            y_pred[i,0] = 0  
     return y_pred
 
 
-mu1, sigma1 = [0], [3] # mean and standard deviation
-mu2, sigma2 = [1], [1] # mean and standard deviation
+def tprfpr_log(actual, predicted, accuracy = False):
+    falseneg, falsepos, truepos, trueneg = 0, 0, 0, 0
+    for i in range(len(actual)):
+        if actual[i] == 0 and predicted[i] == 1 :
+            falsepos += 1
+        if actual[i] == 1 and predicted[i] == 1 :
+            truepos += 1
+        if actual[i] == 1 and predicted[i] == 0 :
+            falseneg += 1
+        if actual[i] == 0 and predicted[i] == 0 :
+            trueneg += 1
+    tpr = truepos / (truepos + falseneg)
+    fpr = falsepos / (falsepos + trueneg)
+    if accuracy:
+        return  tpr, fpr, (truepos+trueneg) / (truepos+trueneg+falsepos+falseneg)
+    else:
+        return tpr, fpr
+    
+def roc_logistic(X, y, testing_data, belonging_classes, list_of_thresholds, how_many_times_repeat):
+    
+    list_fpr = []
+    list_tpr = []
+    list_accuracy = []
+    beta = np.zeros((X.shape[1], 1))
+    beta_0 = 0
+    num_iterations = 100
+    for t in list_of_thresholds:
+        tpr = 0
+        fpr = 0
+        accuracy = 0
+        for repeat in range(how_many_times_repeat):
+            parameters= optimise(X, y, beta, beta_0, num_iterations)
+            predicted_classes =  predict_logistic(testing_data[repeat], parameters["beta"], parameters["beta_0"], t)
+            results = tprfpr_log(belonging_classes[repeat], predicted_classes, accuracy = True)
+            tpr += results[0]
+            fpr += results[1]
+            accuracy += results[2]
+        tpr /= how_many_times_repeat
+        fpr /= how_many_times_repeat
+        accuracy /= how_many_times_repeat
+        
+        list_tpr.append(tpr)
+        list_fpr.append(fpr)
+        list_accuracy.append(accuracy)   
+    
+    return list_tpr, list_fpr, list_accuracy
+    
+
+mu1, sigma1 = [0.25], [1] # mean and standard deviation
+mu2, sigma2 = [1.25], [1] # mean and standard deviation
 
 
 how_many_times_repeat = 1
@@ -84,8 +171,7 @@ testing_data, belonging_classes = generating_test_data(how_many_times_repeat,
 
 ###### First Set of Parameters
 nt1 = 400
-bin_size1 = 0.01
-k1 = 5
+bin_size1 = 2
 bins = np.arange(bin_size1/2,100,bin_size1)
 bins = np.concatenate((-bins[::-1], bins))
 # bins = np.linspace(-100,100,int(200//bin_size))
@@ -94,17 +180,52 @@ s1 = np.random.normal(mu1, sigma1, nt1)
 s2 = np.random.normal(mu2, sigma2, nt1)
 s1_binned = put_in_bins(s1, bins) #binned
 s2_binned = put_in_bins(s2, bins) #binned
-# create X and y to apply kNN
-X = np.concatenate((s1_binned.reshape((nt1,1)),s2_binned.reshape((nt1,1))))
-y = np.concatenate((np.ones((nt1,)),2*np.ones((nt1,))))
 
+# create X and y to apply classification
+X = np.concatenate((s1.reshape((nt1,1)),s2.reshape((nt1,1))))
+y = np.concatenate((0*np.ones((nt1,1)),np.ones((nt1,1))))
 
-
-beta, beta_0 = initialise(X_train.shape[0])
-
-parameters, grads, costs = optimise(X_train, y_train, beta, beta_0, num_iterations, learning_rate)
+# # initialising the parameters
+# beta = np.zeros((X.shape[1], 1))
+# beta_0 = 0
+# num_iterations = 100
+# parameters= optimise(X, y, beta, beta_0, num_iterations)
     
-beta = parameters["beta"]
-beta_0 = parameters["beta_0"]
+# beta_not_binned = parameters["beta"]
+# beta_0_not_binned = parameters["beta_0"]
+
+#### same thing for binned data
+
+X_binned = np.concatenate((s1_binned.reshape((nt1,1)),s2_binned.reshape((nt1,1))))
+y_binned = np.concatenate((0*np.ones((nt1,1)),np.ones((nt1,1))))
+
+
+# parameters= optimise(X_binned, y_binned, beta, beta_0, num_iterations)
     
-y_pred_test = predict_logistic(X_test, beta, beta_0, threshold)
+# beta_binned = parameters["beta"]
+# beta_0_binned = parameters["beta_0"]
+
+# y_pred_not_binned = predict_logistic(testing_data[0], beta_not_binned, beta_0_not_binned, 0.5)
+# y_pred_binned = predict_logistic(testing_data[0], beta_binned, beta_0_binned, 0.5)
+
+
+
+
+
+
+#### ROC curves
+list_tpr, list_fpr, list_accuracy = roc_logistic(X, y, testing_data, belonging_classes, list_of_thresholds, how_many_times_repeat)
+
+list_tpr_b, list_fpr_b, list_accuracy_b = roc_logistic(X_binned, y_binned, testing_data, belonging_classes, list_of_thresholds, how_many_times_repeat)
+
+x = np.linspace(0,1,100)
+plt.figure()
+
+plt.plot(list_fpr,list_tpr,'r.')
+plt.plot(list_fpr_b,list_tpr_b,'y.',)
+plt.plot(x,x)
+plt.legend()
+plt.xlabel('fpr')
+plt.ylabel('tpr')
+# plt.title('$n_t$ = '+str(value_nt) +', $b$ = '+str(value_b))
+plt.show()
