531334957114085d6f85056a03c7dc5434dc9190
Iris-Ioana Roatis <72552991+irisroatis@users.noreply.github.com>
2022-12-13 22:14:06 +0000

    Add files via upload

--- checking_test_statistics_binned.py
+++ checking_test_statistics_binned.py
@@ -10,49 +10,55 @@ import numpy as np
 from defined_functions import *
 import matplotlib.pyplot as plt
 
-mu = 1
+mu = 0
 sigma = 1
 
-list_nt = [5, 50, 100, 500, 1000, 5000, 10000, 50000]
-bin_size = 0.01
+list_nt = [5, 10, 50, 100, 500, 1000, 5000, 10000]
+bin_size = 0.5
 bins = np.arange(bin_size/2,100,bin_size)
 bins = np.concatenate((-bins[::-1], bins))
 # bins = np.linspace(-100,100,int(200//bin_size))
 how_many = 100
 list_mean_binned = []
-list_difference = []
 list_sample_variance = []
 
 for nt in list_nt:
-    sample_mean_binned = np.zeros((how_many,))
-    sample_variance = np.zeros((how_many,))
+    difference_sample_mean = np.zeros((how_many,))
+    difference_sample_variance = np.zeros((how_many,))
     for iterations in range(how_many):
         random_simulation_from_normal =  np.random.normal(mu, sigma, nt)
-
         binned_simulted_data = put_in_bins(random_simulation_from_normal, bins)
-        sample_mean_binned[iterations,] = np.mean(binned_simulted_data)
-        sample_variance[iterations,] = sum((binned_simulted_data -  sample_mean_binned[iterations,])**2) / (nt-1) 
-    list_mean_binned.append(sample_mean_binned)
-    list_difference.append(sample_mean_binned-mu)
-    list_sample_variance.append(sample_variance)
+        mean_binned =  np.mean(binned_simulted_data)
+        mean_unbinned =  np.mean(random_simulation_from_normal) 
+        
+        difference_sample_mean[iterations,] = mean_binned - mean_unbinned
+        difference_sample_variance[iterations,] = sum((binned_simulted_data -  mean_binned)**2) / (nt-1) - sum((random_simulation_from_normal -  mean_unbinned)**2) / (nt-1)
+    list_mean_binned.append(difference_sample_mean)
+    list_sample_variance.append(difference_sample_variance)
     
 fig, ax = plt.subplots()
 ax.boxplot(list_mean_binned)
 ax.set_xticklabels(list_nt)
-plt.axhline(mu,color = 'g',linestyle = '--',linewidth = 1)
-plt.title('Boxplots of estimated means from binned data')
+plt.axhline(0,color = 'g',linestyle = '--',linewidth = 1, label = '0')
+plt.xlabel('size of sample')
+plt.ylabel('$E[X] - E[X^{*}]$')
+plt.title('Boxplots of Difference in Mean of Unbinned Data vs Binned Data')
+plt.legend()
 plt.show()
 
-fig, ax = plt.subplots()
-ax.boxplot(list_difference)
-ax.set_xticklabels(list_nt)
-plt.axhline(0,color = 'g',linestyle = '--',linewidth = 1)
-plt.title('Boxplots of bias of mean')
-plt.show()
+# fig, ax = plt.subplots()
+# ax.boxplot(list_difference)
+# ax.set_xticklabels(list_nt)
+# plt.axhline(0,color = 'g',linestyle = '--',linewidth = 1)
+# plt.title('Boxplots of bias of mean')
+# plt.show()
 
 fig, ax = plt.subplots()
 ax.boxplot(list_sample_variance)
 ax.set_xticklabels(list_nt)
-plt.axhline(sigma + bin_size**2/12,color = 'g',linestyle = '--',linewidth = 1)
-plt.title('Boxplots of sample variances about the estimated mean')
+plt.axhline(bin_size**2/12,color = 'g',linestyle = '--',linewidth = 1, label = '$\\frac{h^2}{12}$')
+plt.xlabel('size of sample')
+plt.ylabel('$V[X] - V[X^{*}]$')
+plt.title('Boxplots of Difference in Variance of Unbinned Data \n about the Mean vs Binned Data')
+plt.legend()
 plt.show()
