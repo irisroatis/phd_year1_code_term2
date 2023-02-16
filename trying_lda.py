0eb100d47d4073c477191bda35894c85f8def9eb
Iris-Ioana Roatis <iris-ioana.roatis18@imperial.ac.uk>
2023-02-16 16:23:10 +0000

    update plots suff stat lda

--- trying_lda.py
+++ trying_lda.py
@@ -16,17 +16,16 @@ from defined_functions import *
 
 size = 1000
 
-# list_bin_sizes = [0.05,0.1,0.5, 1, 1.5, 2.75, 3.3, 3.6, 4]
+list_bin_sizes = np.linspace(0.01,5,100)
 
-list_bin_sizes = [0.01,0.05,0.1,0.25, 0.35, 0.5, 0.65, 0.75, 0.9, 1, 1.25, 1.5, 2]
+# list_bin_sizes = [0.01,0.05,0.1,0.25, 0.35, 0.5, 0.65, 0.75, 0.9, 1, 1.25, 1.5, 2]
 
-how_many = 200
+how_many = 100
 
 accuracy = [np.zeros((how_many,))]
 accuracy_on_binned = [np.zeros((how_many,))]
 store_maha_dist = []
 store_bhatt_dist = []
-
 
 
 mu1, sigma1 = [0.5], [1] # mean and standard deviation
@@ -49,7 +48,7 @@ for i in range(len(list_bin_sizes)):
     store_bhatt_dist.append(np.zeros((how_many,)))
 
     bin_size = list_bin_sizes[i]
-    bins = np.arange(bin_size/2,200,bin_size)
+    bins = np.arange(bin_size/2,1000,bin_size)
     bins = np.concatenate((-bins[::-1], bins))
     list_of_bins.append(bins)
     
@@ -91,6 +90,8 @@ for iteration in range(how_many):
 
 dict_accuracy = {'no \n binning':accuracy[0]}
 dict_accuracy_b = {'no \n binning':accuracy_on_binned[0]}
+dict_accuracy_mean = [np.mean(accuracy[0])]
+dict_accuracy_b_mean = [np.mean(accuracy_on_binned[0])]
 dict_maha_dist = {}
 dict_bhatt_dist = {}
 dict_maha_dist_mean = []
@@ -101,45 +102,47 @@ for i in range(len(list_bin_sizes)):
 
     dict_accuracy[str(list_bin_sizes[i])] = accuracy[i+1]
     dict_accuracy_b[str(list_bin_sizes[i])] = accuracy_on_binned[i+1]
+    dict_accuracy_mean.append(np.mean(accuracy[i+1]))
+    dict_accuracy_b_mean.append(np.mean(accuracy_on_binned[i+1]))
     dict_maha_dist[str(list_bin_sizes[i])] = store_maha_dist[i]
     dict_bhatt_dist[str(list_bin_sizes[i])] = store_bhatt_dist[i]
     dict_maha_dist_mean.append(np.mean(store_maha_dist[i]))
     dict_bhatt_dist_mean.append(np.mean(store_bhatt_dist[i]))
  
 
-# Pandas dataframe
-data1 = pd.DataFrame(dict_accuracy)
-# Plot the dataframe
-ax = data1[['no \n binning','0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
-plt.xlabel('bin size, $h$')
-plt.ylabel('accuracy')
-plt.title('Accuracy LDA')
-plt.show()
+# # Pandas dataframe
+# data1 = pd.DataFrame(dict_accuracy)
+# # Plot the dataframe
+# ax = data1[['no \n binning','0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
+# plt.xlabel('bin size, $h$')
+# plt.ylabel('accuracy')
+# plt.title('Accuracy LDA')
+# plt.show()
 
-data2 = pd.DataFrame(dict_accuracy_b)
-# Plot the dataframe
-ax = data2[['no \n binning','0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
-plt.xlabel('bin size, $h$')
-plt.ylabel('accuracy')
-plt.title('Accuracy LDA')
-plt.show()
+# data2 = pd.DataFrame(dict_accuracy_b)
+# # Plot the dataframe
+# ax = data2[['no \n binning','0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
+# plt.xlabel('bin size, $h$')
+# plt.ylabel('accuracy')
+# plt.title('Accuracy LDA')
+# plt.show()
 
 
-data3 = pd.DataFrame(dict_maha_dist)
-# Plot the dataframe
-ax = data3[['0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
-plt.xlabel('bin size, $h$')
-plt.ylabel('$D_{ma}(p,q)$')
-plt.title('Mahalanobis Distance')
-plt.show()
+# data3 = pd.DataFrame(dict_maha_dist)
+# # Plot the dataframe
+# ax = data3[['0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
+# plt.xlabel('bin size, $h$')
+# plt.ylabel('$D_{ma}(p,q)$')
+# plt.title('Mahalanobis Distance')
+# plt.show()
 
-data4 = pd.DataFrame(dict_bhatt_dist)
-# Plot the dataframe
-ax = data4[['0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
-plt.xlabel('bin size, $h$')
-plt.ylabel('$D_{bh}(p,q)$')
-plt.title('Bhattacharya Distance')
-plt.show()
+# data4 = pd.DataFrame(dict_bhatt_dist)
+# # Plot the dataframe
+# ax = data4[['0.01','0.05','0.1','0.25', '0.35', '0.5', '0.65', '0.75', '0.9', '1', '1.25', '1.5','2']].plot(kind='box', title='boxplot')
+# plt.xlabel('bin size, $h$')
+# plt.ylabel('$D_{bh}(p,q)$')
+# plt.title('Bhattacharya Distance')
+# plt.show()
 
 plt.scatter(list(list_bin_sizes), dict_maha_dist_mean)
 plt.title('Mahalanobis Distance')
@@ -152,6 +155,32 @@ plt.title('Bhattacharya Distance')
 plt.xlabel('bin size, $h$')
 plt.ylabel('$E[D_{bh}(p,q)]$')
 plt.show()
+
+# plt.scatter([0] + list(list_bin_sizes), dict_accuracy_mean)
+# plt.title('Non-binned Test Data')
+# plt.xlabel('bin size, $h$')
+# plt.ylabel('$E[$Accuracy$]$')
+# plt.show()
+
+# plt.scatter([0] + list(list_bin_sizes), dict_accuracy_b_mean)
+# plt.title('Binned Test Data')
+# plt.xlabel('bin size, $h$')
+# plt.ylabel('$E[$Accuracy$]$')
+# plt.show()
+
+plt.scatter([0] + dict_bhatt_dist_mean, dict_accuracy_b_mean)
+plt.title('Binned Test Data, Bhattacharya')
+plt.ylabel('$E[$Accuracy$]$')
+plt.xlabel('E[D_{bh}(p,q)]')
+plt.show()
+
+plt.scatter([0] + dict_maha_dist_mean, dict_accuracy_b_mean)
+plt.title('Binned Test Data, Mahalanobis')
+plt.ylabel('$E[$Accuracy$]$')
+plt.xlabel('E[D_{ma}(p,q)]')
+plt.show()
+
+
 
 
 
